069e0dbe24cd8619a1f3f45749887d89bb20ec84
Brian Zanti <brianzanti@gmail.com>
2018-05-30 12:03:06 -0600

    completed code

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,45 @@
 class Activity
+  attr_reader :name,
+              :base_cost,
+              :individual_cost,
+              :participants
+
+  def initialize(name, base_cost, individual_cost)
+    @name = name
+    @base_cost = base_cost
+    @individual_cost = individual_cost
+    @participants = []
+  end
+
+  def add_participant(name, cost)
+    new_participant = {name: name, paid: cost}
+    @participants << new_participant
+  end
+
+  def num_participants
+    @participants.length
+  end
+
+  def total_cost
+    @base_cost + num_participants * @individual_cost
+  end
+
+  def fair_share
+    (total_cost.to_f / num_participants.to_f).round(2)
+  end
+
+  def amount_owed(name)
+    participant = @participants.find do |participant|
+      participant[:name] == name
+    end
+    (fair_share - participant[:paid]).round(2)
+  end
+
+  def debts
+    @participants.inject({}) do |debts, participant|
+      name = participant[:name]
+      require 'pry'; binding.pry
+      debts[name] = amount_owed(name)
+    end
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,62 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def setup
+    @activity = Activity.new("Camping", 50, 10)
+  end
+
+  def test_it_has_attributes
+    assert_equal "Camping", @activity.name
+    assert_equal 50, @activity.base_cost
+    assert_equal 10, @activity.individual_cost
+  end
+
+  def test_participants_starts_empty
+    assert_equal [], @activity.participants
+  end
+
+  def test_add_participants
+    @activity.add_participant("Brian", 20)
+    @activity.add_participant("Megan", 5)
+
+    participant_1 = {name: "Brian", paid: 20}
+    participant_2 = {name: "Megan", paid: 5}
+    assert_equal [participant_1, participant_2], @activity.participants
+  end
+
+  def test_total_cost
+    @activity.add_participant("Brian", 20)
+    @activity.add_participant("Megan", 5)
+
+    assert_equal 70.00, @activity.total_cost
+  end
+
+  def test_fair_share
+    @activity.add_participant("Brian", 20)
+    @activity.add_participant("Megan", 5)
+    @activity.add_participant("Scott", 1)
+
+    assert_equal 26.67, @activity.fair_share
+  end
+
+  def test_amount_owed
+    @activity.add_participant("Brian", 30)
+    @activity.add_participant("Megan", 5)
+    @activity.add_participant("Scott", 1)
+    assert_equal -3.33, @activity.amount_owed("Brian")
+    assert_equal 21.67, @activity.amount_owed("Megan")
+  end
+
+  def test_debts
+    @activity.add_participant("Brian", 30)
+    @activity.add_participant("Megan", 5)
+    @activity.add_participant("Scott", 1)
+    expected = {
+      "Brian" => -3.33,
+      "Megan" => 21.67,
+      "Scott" => 25.67
+    }
+    assert_equal expected, @activity.debts
+  end
 end
